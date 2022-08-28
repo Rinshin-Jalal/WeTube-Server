7772765e55a6ba97c3559ad48294e5e4af527ae7
Aarul Aggarwal <aggarwalaarul@gmail.com>
2022-08-28 10:53:11 +0530

    navbar menu bar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,15 +4,18 @@ import logo from '/images/logo.png'
 import upload from '/images/upload.png'
 import search from '/images/search.png'
 import userimg from '/images/user.png'
+import menu from '/images/menu.png'
 
 const Navbar = () => {
     const [user, setUser] = useState(false)
+    const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <div className='nav'>
         <div className='nav-logo'>
             <img src={logo} alt='logo' />
         </div>
-        <div class="wrap">
+        <div className={menuOpen ? 'wrap-expanded' : 'wrap'}>
             <div class="search">
                 <input type="text" class="searchTerm" placeholder="Search for videoz" />
                 <button type="submit" class="searchButton">
@@ -20,16 +23,21 @@ const Navbar = () => {
                 </button>
             </div>
         </div>
-        <div className='nav-links'>
-            <img src={upload} alt='upload video' />
+        <button className='menu' onClick={() => setMenuOpen(!menuOpen)} ><img src={menu} /></button>
+        <div className={menuOpen ? 'menu-links' : 'nav-links'}>
+            <div className={ menuOpen ? 'menu-link' : ''}>
+                <img src={upload} alt='upload video' />
+                <h1>Upload Video</h1>
+            </div>
             {
                 user ? 
                     <div className='user'>
                         <img src={userimg} alt='user' />
                         <h3>User</h3>
-                    </div> : 
-                    <div className='login'>
-                        <button type='submit' onClick={() => setUser(true)}>Login</button>
+                    </div> 
+                : 
+                    <div className={menuOpen ? 'menu-login' : 'login'}>
+                        <button type='submit' onClick={() => setUser(true)}>Sign In</button>
                     </div>
             }
         </div>
